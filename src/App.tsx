c153c546b3caa1daafc75e14e37b8663a1b16830
Guilherme Lima <guilhermelso2005@gmail.com>
2025-05-27 18:50:04 -0300

    feito todos os metodos de tema

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,31 @@ import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import { AuthProvider } from './contexts/AuthContext';
 import Cadastro from './pages/cadastro/Cadastro';
+import ListaTemas from './components/temas/listatemas/ListaTemas';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import FormTema from './components/temas/formtema/FormTema';
+import DeletarTema from './components/temas/deletartema/DeletarTema';
 
 function App() {
 
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/cadastro" element= {<Cadastro/>}/>
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Navbar />
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element= {<Cadastro/>}/>
+            <Route path="/login" element={<Login />} />
+            <Route path="/temas" element={<ListaTemas />} />
+            <Route path="/cadastrartema" element={<FormTema />} />
+            <Route path="/editartema/:id" element={<FormTema />} />
+            <Route path="/deletartema/:id" element={<DeletarTema />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </AuthProvider>
   );
